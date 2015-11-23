0b9f8cab29acf5d868d434e2a942f30e17851672
Andrei Gherzan <andrei@resin.io>
2015-11-24 00:34:48 +0100

    prepare.sh: Stop containers with rce and add hint how to start resinhup
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- prepare.sh
+++ prepare.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-echo "Stopping resin-supervisor"
-systemctl stop resin-supervisor
+echo "Stopping all containers"
+rce stop $(rce ps -a -q)
 
 echo "Building the image"
 rce build -t resinhup .
+
+echo "Run container using:"
+echo "rce run -ti --privileged --rm --net=host --volume /:/host resinhup /bin/bash"
 
 echo "Done"
