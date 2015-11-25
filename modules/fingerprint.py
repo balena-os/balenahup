d58db57e95f0fe820d7778e4179af84fbb70692b
Andrei Gherzan <andrei@resin.io>
2015-11-25 23:10:50 +0100

    fingerprint.py: Improve testing and other minor changes
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- modules/fingerprint.py
+++ modules/fingerprint.py
@@ -24,30 +24,46 @@ class FingerPrintScanner(object):
         self.conf = conf
 
     def scan(self):
+        log.info("FingerPrintScanner: Started to scan for fingerprints... this will take a while...")
         whitelist_fingerprints = getConfigurationItem(self.conf, "FingerPrintScanner", "whitelist").split()
         for root, dirs, files in os.walk(self.root, followlinks=False):
             if self.skipMountPoints:
-                # Filter out from dirs the mountpoints - stay on same filesystem
-                dirs[:] = filter(lambda dir: not os.path.ismount(os.path.join(root, dir)), dirs)
-                # Filter out whitelist
-                dirs[:] = filter(lambda dir: not os.path.join(root, dir) in whitelist_fingerprints, dirs)
+                if log.getEffectiveLevel() == logging.DEBUG:
+                    # Filter out from dirs the mountpoints = stay on same filesystem
+                    temp_dirs = filter(lambda dir: not os.path.ismount(os.path.join(root, dir)), dirs)
+                    if set(dirs) != set(temp_dirs):
+                        log.debug("FingerPrintScanner: Ignored these directories as they were mountpoint: " + ', '.join(set(dirs) - set(temp_dirs)))
+                    dirs[:] = temp_dirs[:]
+                    # Filter out whitelist
+                    temp_dirs = filter(lambda dir: not os.path.join(root, dir) in whitelist_fingerprints, dirs)
+                    if set(dirs) != set(temp_dirs):
+                        log.debug("FingerPrintScanner: Ignored these directories as they were whitelisted: " + ', '.join(set(dirs) - set(temp_dirs)))
+                    dirs[:] = temp_dirs[:]
+                else:
+                    # Same as above but without debug
+                    dirs[:] = filter(lambda dir: not os.path.ismount(os.path.join(root, dir)), dirs)
+                    dirs[:] = filter(lambda dir: not os.path.join(root, dir) in whitelist_fingerprints, dirs)
             for filename in files:
                 if os.path.islink(os.path.join(root,filename)):
                     continue
                 if not os.path.isfile(os.path.join(root,filename)):
                     continue
-                if os.path.isfile(os.path.join(root,filename)) in whitelist_fingerprints:
+                if os.path.join(os.path.join(root,filename)) in whitelist_fingerprints:
+                    log.debug("FingerPrintScanner: Ignored " + os.path.join(root,filename) + " as it was found whitelisted.")
                     continue
                 with open(os.path.join(root,filename)) as f:
                     filecontent = f.read()
                     filemd5 = hashlib.md5(filecontent).hexdigest()
                     self.fingerprints[os.path.join(root, filename)] = filemd5
-    def getFingerPrints(self):
+    def printFingerPrints(self):
         fingerprints = "# File MD5SUM\t\t\t\tFilepath\n"
         sorted_fingerprints = sorted(self.fingerprints.items(), key=operator.itemgetter(0))
         for filename, filemd5 in sorted_fingerprints:
             fingerprints += filemd5 + "\t" + filename + "\n"
         return fingerprints
+
+    def getFingerPrints(self):
+        return self.fingerprints
 
     def validateFingerPrints(self):
         toReturn = True
@@ -81,12 +97,31 @@ class MyTest(unittest.TestCase):
         ch.setFormatter(ColoredFormatter(True))
         log.addHandler(ch)
 
-        conf = "../conf/resinhup"
-        scanner = FingerPrintScanner("/var/lib/iptables", conf)
-        scanner.scan()
-        print scanner.getFingerPrints()
+        # Test that it ignores mountpoints
+        mountpoint = "./fingerprint/tests/testRun/tree/dir1"
+        mount(what="tmpfs", where=mountpoint, mounttype="tmpfs")
 
-        print scanner.validateFingerPrints()
+        conf = "./fingerprint/tests/testRun/resinhup"
+        scanner = FingerPrintScanner("./fingerprint/tests/testRun/tree", conf)
+        scanner.scan()
+
+        # Cleanup mount
+        umount(mountpoint)
+
+        print scanner.printFingerPrints()
+
+        whitelist_fingerprints = getConfigurationItem(conf, "FingerPrintScanner", "whitelist").split()
+        fingerprints = scanner.getFingerPrints()
+
+        # Check on known file
+        self.assertTrue(fingerprints['./fingerprint/tests/testRun/tree/dir4/file1'] == '68b329da9893e34099c7d8ad5cb9c940')
+
+        for filename,filemd5 in fingerprints.items():
+            self.assertFalse(filename in whitelist_fingerprints)
+            # Check mountpoint
+            self.assertFalse(filename.startswith(mountpoint))
+
+        self.assertTrue(scanner.validateFingerPrints())
 
 if __name__ == '__main__':
     unittest.main()
