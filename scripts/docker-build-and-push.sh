d37cb19ef62c8999175f13b2ad11677ad5bbde6d
Andrei Gherzan <andrei@resin.io>
2016-05-04 12:58:04 +0200

    docker-build-and-push.sh: Add argument to push only one Dockerfile
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- scripts/docker-build-and-push.sh
+++ scripts/docker-build-and-push.sh
@@ -5,12 +5,58 @@ set -e
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# Help function
+function help {
+    cat << EOF
+Run docker build and push for the supported images.
+$0 <OPTION>
+
+Options:
+  -h, --help
+        Display this help and exit.
+
+  -d, --dockerfile
+        Build and push only this Dockerfile. Otherwise all found will be used.
+EOF
+}
+
+#
+# MAIN
+#
+
+# Parse arguments
+while [[ $# > 0 ]]; do
+    arg="$1"
+
+    case $arg in
+        -h|--help)
+            help
+            exit 0
+            ;;
+        -d|--dockerfile)
+            if [ -z "$2" ]; then
+                log ERROR "\"$1\" argument needs a value."
+            fi
+            DOCKERFILES=$2
+            shift
+            ;;
+        *)
+            log ERROR "Unrecognized option $1."
+            ;;
+    esac
+    shift
+done
+
 # Get the absolute script location
 pushd `dirname $0` > /dev/null 2>&1
 SCRIPTPATH=`pwd`
 popd > /dev/null 2>&1
 
-for dockerfile in `ls $SCRIPTPATH/../Dockerfile.*`; do
+if [ -z "$DOCKERFILES" ]; then
+    DOCKERFILES=$(ls $SCRIPTPATH/../Dockerfile.*)
+fi
+
+for dockerfile in $DOCKERFILES; do
     dockerfile=$(basename $dockerfile)
     device=$(echo $dockerfile | cut --delimiter '.' -f2)
     if [ -z "$device" ]; then
