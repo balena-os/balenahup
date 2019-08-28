eff1ae71e4f9f8da9584764b3b907490cd51eb57
Gergely Imreh <gergely@balena.io>
2019-08-28 15:15:41 +0100

    Remove "--nolog" flag from 2.x updater, and always set up logfile
    Previously we might have logged when there wasn't any logfile setup,
    for example if any of the argument parsing was throwing an error,
    and on the 2.x updater there wasn't any way to directly know what
    the error was, just that the update didn't start.
    
    Moving the logfile setup before any logging takes place eliminates
    this issue. The flag was also only really used in manual HUP runs
    (no way to use in self-serve updates), and it's more relevant to
    1.x updates than anything since.
    
    Change-type: minor
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- upgrade-ssh-2.x.sh
+++ upgrade-ssh-2.x.sh
@@ -74,12 +74,6 @@ Options:
         Run ${main_script_name} with --assume-supported
         See ${main_script_name} help for more details.
 
-  --nolog
-        Run ${main_script_name} with --nolog
-        See ${main_script_name} help for more details. For running over ssh this is likely
-        recommended, as otherwise the log is just kept on the device, the local log
-        on the computer running the remote updater script will have only log headers.
-
   --no-reboot
         Run ${main_script_name} with --no-reboot . See ${main_script_name} help for more details.
 
@@ -294,9 +288,6 @@ while [[ $# -gt 0 ]]; do
             ;;
         --no-reboot)
             RESINHUP_ARGS+=( "--no-reboot" )
-            ;;
-        --nolog)
-            RESINHUP_ARGS+=( "--nolog" )
             ;;
         --no-colors)
             NOCOLORS=yes
