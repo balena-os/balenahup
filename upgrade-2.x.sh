bd7f482c18544231b3f82e676823db481694b6fc
Matthew McGinn <matthew@balena.io>
2020-06-05 09:56:39 -0400

    private dts: enable HUP for access-restricted device types
    Change-type: minor
    Signed-off-by: Matthew McGinn <matthew@balena.io>

--- upgrade-2.x.sh
+++ upgrade-2.x.sh
@@ -674,7 +674,7 @@ function get_image_location() {
     image=$(CURL_CA_BUNDLE=${TMPCRT} curl --retry 10 --silent -X GET \
         -H "Content-Type: application/json" \
         -H "Authorization: Bearer ${APIKEY}" \
-        "${API_ENDPOINT}/v5/release?\$expand=release_tag,belongs_to__application,contains__image/image&\$filter=(belongs_to__application/any(a:a/device_type%20eq%20'${SLUG}'))%20and%20(release_tag/any(rt:(rt/tag_key%20eq%20'version')%20and%20(rt/value%20eq%20'${version}')))" \
+        "${API_ENDPOINT}/v5/release?\$expand=release_tag,belongs_to__application,contains__image/image&\$filter=(belongs_to__application/any(a:a/device_type%20eq%20'${SLUG}'%20and%20is_host%20eq%20true))%20and%20(release_tag/any(rt:(rt/tag_key%20eq%20'version')%20and%20(rt/value%20eq%20'${version}')))" \
         | jq -r "[.d[] | select(.release_tag[].value == (\"${VARIANT}\" | ascii_downcase)) | .contains__image[0].image[0] | [.is_stored_at__image_location, .content_hash] | \"\(.[0])@\(.[1])\"]")
     if echo "${image}" | jq -e '. | length == 1' > /dev/null; then
         echo "${image}" | jq -r '.[0]'
@@ -1123,6 +1123,9 @@ if version_gt "${VERSION_ID}" "${minimum_hostapp_target_version}" ||
     # record the "source" of each image in the array above for clarity during fallback
     image_types=("delta" "balena_registry")
     update_failed=0
+    # login for private dts
+    DOCKER_HOST="unix:///var/run/${DOCKER_CMD}-host.sock" ${DOCKER_CMD} login "${REGISTRY_ENDPOINT}" -u "d_${UUID}" \
+    --password "${APIKEY}" > /dev/null 2>&1 || log WARN "logging into registry failed, proceeding anyway (only required for private dts)"
     for img in "${images[@]}"; do
         if [ -n "${img}" ] && hostapp_based_update "${img}"; then
             # once we've updated successfully, set our canonical image
@@ -1136,6 +1139,7 @@ if version_gt "${VERSION_ID}" "${minimum_hostapp_target_version}" ||
     if [ -z "${image}" ]; then
         log ERROR "all hostapp-update attempts have failed..."
     fi
+    DOCKER_HOST="unix:///var/run/${DOCKER_CMD}-host.sock" ${DOCKER_CMD} logout "${REGISTRY_URL}" > /dev/null 2>&1
 
     if [ "${LEGACY_UPDATE}" = "yes" ]; then
         upgrade_supervisor "${image}" no_docker_host
