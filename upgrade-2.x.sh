f90ae39840fc62e154ed7fb5c8bedb59e8c86d34
Pagan Gazzard <page@balena.io>
2025-09-05 12:32:21 +0100

    balena-api: avoid unnecessary `eq true`
    Change-type: patch

--- upgrade-2.x.sh
+++ upgrade-2.x.sh
@@ -949,7 +949,7 @@ function get_image_location() {
     image=$(CURL_CA_BUNDLE="${TMPCRT}" ${CURL} \
         -H "Content-Type: application/json" \
         -H "Authorization: Bearer ${APIKEY}" \
-        "${API_ENDPOINT}/v6/release?\$select=id&\$expand=contains__image/image&\$filter=belongs_to__application/any(a:a/is_for__device_type/any(dt:dt/slug%20eq%20'${SLUG}')%20and%20is_host%20eq%20true)%20and%20is_invalidated%20eq%20false%20and%20raw_version%20eq%20'${version}'" \
+        "${API_ENDPOINT}/v6/release?\$select=id&\$expand=contains__image/image&\$filter=belongs_to__application/any(a:a/is_for__device_type/any(dt:dt/slug%20eq%20'${SLUG}')%20and%20is_host)%20and%20is_invalidated%20eq%20false%20and%20raw_version%20eq%20'${version}'" \
         | jq -r "[.d[] | .contains__image[0].image[0] | [.is_stored_at__image_location, .content_hash] | \"\(.[0])@\(.[1])\"]")
     if echo "${image}" | jq -e '. | length == 1' > /dev/null; then
         echo "${image}" | jq -r '.[0]'
@@ -960,7 +960,7 @@ function get_image_location() {
         image=$(CURL_CA_BUNDLE="${TMPCRT}" ${CURL} \
             -H "Content-Type: application/json" \
             -H "Authorization: Bearer ${APIKEY}" \
-            "${API_ENDPOINT}/v6/release?\$select=id&\$expand=contains__image/image&\$filter=belongs_to__application/any(a:a/is_for__device_type/any(dt:dt/slug%20eq%20'${SLUG}')%20and%20is_host%20eq%20true)%20and%20is_final%20eq%20true%20and%20is_invalidated%20eq%20false%20and%20release_tag/any(rt:rt/tag_key%20eq%20'version'%20and%20rt/value%20eq%20'${version}')%20and%20(release_tag/any(rt:rt/tag_key%20eq%20'variant'%20and%20rt/value%20eq%20'${variant_tag}')%20or%20not%20release_tag/any(rt:rt/tag_key%20eq%20'variant'))" \
+            "${API_ENDPOINT}/v6/release?\$select=id&\$expand=contains__image/image&\$filter=belongs_to__application/any(a:a/is_for__device_type/any(dt:dt/slug%20eq%20'${SLUG}')%20and%20is_host)%20and%20is_final%20and%20is_invalidated%20eq%20false%20and%20release_tag/any(rt:rt/tag_key%20eq%20'version'%20and%20rt/value%20eq%20'${version}')%20and%20(release_tag/any(rt:rt/tag_key%20eq%20'variant'%20and%20rt/value%20eq%20'${variant_tag}')%20or%20not%20release_tag/any(rt:rt/tag_key%20eq%20'variant'))" \
             | jq -r "[.d[] | .contains__image[0].image[0] | [.is_stored_at__image_location, .content_hash] | \"\(.[0])@\(.[1])\"]")
         if echo "${image}" | jq -e '. | length == 1' > /dev/null; then
             echo "${image}" | jq -r '.[0]'
